d14a889933ffedd299cbd8f3ff5d56af5d64ab50
DevWesleyMatos <wesley.matos.ti@gmail.com>
2025-07-16 22:03:28 -0300

    aula 16-07

---
+++ AULA-16-07/exercicio.js
@@ -0,0 +1,16 @@
+// let pessoa = prompt("Digite");
+// let produto1 = "vassoura";
+// let produto2 = "sabão";
+// let produto3 = "oleo";
+// let transporte = "uber";
+// let saldoTotal = 80;
+// let gastoSuper = 48;
+// let gastoTotal = saldoTotal - gastoSuper;
+
+// console.log(
+//   `${pessoa} foi ao supermercado comprar ${produto1} mas tbm esqueceu ${produto2} e depois voltou e comprou ${produto3},ele gastou no supermercado $${gastoSuper} ele veio de ${transporte}, e no  ele ficou com somente $${gastoTotal}`
+// );
+let valor1 = 10
+let valor2 = 10
+
+console.log(valor1 + valor2)
---
+++ AULA-16-07/script.js
@@ -0,0 +1,25 @@
+
+
+console.log("ola")
+
+let a = 2;
+let b = 9;
+let c = prompt
+
+console.log(a + b)
+
+if (a === b) {
+    console.log("DEU CERTO!!")
+} else {
+    console.log("Não deu certto")
+}
+
+for (a < b; a.length; a++){
+   
+}   
+
+
+function test() {
+    let a = 
+    alert(a + "kjsnknakcasnc")
+}
