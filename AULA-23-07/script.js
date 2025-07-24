31ead47fabb533d968ae818923ac502212218b37
DevWesleyMatos <wesley.matos.ti@gmail.com>
2025-07-23 22:03:11 -0300

    AULA-23-07

---
+++ AULA-23-07/script.js
@@ -0,0 +1,52 @@
+let carros = [
+  ["Toyota", "Corolla", 2022, "Gasolina"],
+  ["Honda", "Civic", 2021, "Flex"],
+  ["Ford", "Mustang", 2020, "Gasolina"],
+  ["Chevrolet", "Onix", 2023, "Flex"],
+  ["Volkswagen", "Golf", 2019, "Diesel"],
+  ["Fiat", "Argo", 2022, "Flex"],
+  ["Hyundai", "HB20", 2021, "Flex"],
+  ["Renault", "Kwid", 2023, "Gasolina"],
+  ["BMW", "320i", 2022, "Gasolina"],
+  ["Jeep", "Compass", 2024, "Diesel"],
+];
+
+// Gostaria que vocês mostrassem no console a seguintes frases:
+console.log(
+  `O carro ${carros[9][0]} ${carros[9][1]} ano ${carros[9][2]} e uma bomba`
+);
+// O carro Jeep Compass ano 2024 é uma bomba
+
+console.log(`o carro`)
+// A BMW 320i é um carro movido a gasolina
+
+// O fiat Argo está de promoção, 2 salgado e uma coca tu leva
+
+// Toyota corolla é um carro de jovem nascidos em 1978
+
+const universidades = [
+  [
+    ["USP", "São Paulo", 1934],
+    ["UFPE", "Recife", 1946]
+  ],
+  [
+    ["Harvard", "Cambridge", 1636],
+    ["MIT", "Cambridge", 1861]
+  ],
+  [
+    ["Universidade de Tóquio", "Tóquio", 1877],
+    ["Universidade de Kyoto", "Kyoto", 1897]
+  ]
+];
+
+
+function cidade_ano() {
+    
+}
+
+
+// Mostre a cidade e o ano de fundação da MIT.
+
+// Qual é a universidade mais antiga do Japão da lista?
+
+// Qual universidade brasileira está localizada em Recife?
