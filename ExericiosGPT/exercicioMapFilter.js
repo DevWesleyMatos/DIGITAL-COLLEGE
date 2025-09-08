12f9f9f79b44608fbdf89125e8c9996fcab11eea
DevWesleyMatos <wesley.matos.ti@gmail.com>
2025-09-08 07:27:55 -0300

    Consumindo Api

---
+++ ExericiosGPT/exercicioMapFilter.js
@@ -0,0 +1,28 @@
+// Use filter para pegar apenas os números pares.
+// Depois use map para dobrar esses números.
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+
+pegaPares = numeros.filter(function(numero) {
+    return numero%2 ===0
+})
+console.log(pegaPares);
+dobrarPares = pegaPares.map(function (numero) {
+    return numero*2
+})
+console.log(dobrarPares);
+
+
+// Filtre os nomes com até 4 letras.
+// Depois transforme-os em maiúsculas.
+let nomes = ["Ana", "Carlos", "Beatriz", "João", "Rafa"];
+
+nomes4letras = nomes.filter(function (nome) {
+    return nome.length <=4
+})
+console.log(nomes4letras);
+
+nomesMaiusculo = nomes4letras.map(function(maiuscula) {
+    return maiuscula = maiuscula.toUpperCase()
+})
+console.log(nomesMaiusculo);
+
